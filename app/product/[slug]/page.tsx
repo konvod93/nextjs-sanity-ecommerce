0b37fa0db404d1210f6f7ba433e558f6484008f5
Konstantin <57582571+konvod93@users.noreply.github.com>
2024-03-10 20:05:36 +0200

    product page: created query

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,4 +1,25 @@
-export default function ProductPage() {
+import { client } from "@/app/lib/sanity";
+
+async function getData(slug: string) {
+    const query = `
+    *[_type == "product" && slug.current == "${slug}"][0] {
+        _id,
+          images,
+          price,
+          name,
+          description,
+          "slug": slug.current,
+          "categoryName": category->name
+      }
+    `;
+
+    const data = await client.fetch(query);
+
+    return data;
+}
+
+export default async function ProductPage({ params, }: {params: {slug: string}}) {
+    const data = await getData(params.slug);
     return (
         <h1>Hello from Product Page</h1>
     )
