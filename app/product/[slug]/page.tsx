7e7fc445cde68c2ed5075ef1b1812d9cb9529679
Konstantin <57582571+konvod93@users.noreply.github.com>
2024-03-14 19:55:05 +0200

    creating product page: completed ImageGalery component

--- app/product/[slug]/page.tsx
+++ app/product/[slug]/page.tsx
@@ -1,3 +1,4 @@
+import ImageGallery from "@/app/components/ImageGallery";
 import { fullProduct } from "@/app/interface";
 import { client } from "@/app/lib/sanity";
 
@@ -22,6 +23,22 @@ async function getData(slug: string) {
 export default async function ProductPage({ params, }: {params: {slug: string}}) {
     const data: fullProduct = await getData(params.slug);
     return (
-        <h1>Hello from Product Page</h1>
+        <div className="bg-white">
+            <div className="mx-auto max-w-screen-xl px-4 md:px-8">
+                <div className="grid gap-8 md:grid-cols-2 ">
+                    <ImageGallery images={data.images} />
+
+                    <div className="md:py-8">
+                        <div className="mb-2 md:mb-3">
+                            <span className="mb-0.5 inline-block text-gray-500">
+                                {data.categoryName}
+                            </span>
+                            <h2 className="text-2xl font-bold text-gray-800 lg:text-3xl">{data.name}</h2>
+                        </div>
+                        <div className="mb-6 flex items-center gap-3 md:mb-10"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
     )
 }
